7a27720c5f3c7b8d8c4e23309263444f41bc026e
ilyapolovinkin <2190055@gmail.com>
2025-01-28 12:18:38 +0300

    routes.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,7 +9,7 @@ func (app *application) routes(staticDir *string) *http.ServeMux {
 	mux := http.NewServeMux()
 	root, _ := findRootDir()
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir(filepath.Join(root, *staticDir))})
-	mux.Handle("/gameplatform/static/", http.StripPrefix("/gameplaform/static", fileServer))
+	mux.Handle("/gameplatform/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/gameplatform", app.home)
 	mux.HandleFunc("/gameplatform/game", app.showGamePreview)
 	mux.HandleFunc("/gameplatform/play", app.showGame)
